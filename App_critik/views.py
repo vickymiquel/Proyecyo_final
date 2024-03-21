faba2a73efebd894889c61bc67b01b015da972aa
MarcosMatascuso <matascuso05@gmail.com>
2024-03-21 15:18:48 -0300

    Primer commit

--- App_critik/views.py
+++ App_critik/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import *
+from django.contrib.auth import *
+from App_critik.models import *
+from App_critik.forms import *
+
 def home(request):
     return render(request, "App_critik/home.html")
 
@@ -14,3 +20,32 @@ def about(request):
 
 def profile(requiest):
     pass
+
+def login(request):
+    
+    if request.method == "POST":
+        
+        form = AuthenticationForm(request, data = request.POST)
+        
+        if form.is_valid():
+            
+            usuario = form.cleaned_data.get("username")
+            contraseña = form.cleaned_data.get("password")
+            
+            user = authenticate(username = usuario, password = contraseña)
+            
+            if user:
+                
+                login(request, user)
+                
+                return render(request, "url")
+            
+        else:
+            
+            return render(request, "url", {"mensaje": "Datos incorrectos"})
+        
+    else:
+        
+        form = AuthenticationForm()
+        
+    return render(request, "login.html", {"form"})
