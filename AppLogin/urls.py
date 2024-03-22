8c7f38422a9c91be7957614db7bcddd7fbf6edd7
MarcosMatascuso <matascuso05@gmail.com>
2024-03-22 19:09:35 -0300

    AppLogin casi finalizada

--- AppLogin/urls.py
+++ AppLogin/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from AppLogin.views import *
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path("login/", login, name = "login"),
-    path("signup/", signup, name = "signup")
+    path("signup/", signup, name = "signup"),
+    path("logout", LogoutView.as_view(template_name="AppLogin/logout.html"), name = "logout")
 ]
